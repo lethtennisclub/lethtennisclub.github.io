d41d3a05fba6e6ac279789114be325a4c2da224b
yanafetisova <yana.fetisova271@gmail.com>
2026-05-05 20:50:30 -0600

    update admin calendar

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ async function loadEvents() {
   snap.forEach(d => {
     const data = d.data();
 
+    if (!data.start || !data.end) return;
+
     events.push({
       id: d.id,
       title: data.title,
-      start: data.start,
-      end: data.end
+      start: new Date(data.start).toISOString(),
+      end: new Date(data.end).toISOString()
     });
   });
 
@@ -26,6 +28,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     {
       initialView: 'dayGridMonth',
       height: 650,
+      
+      eventTimeFormat: {
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false
+},
 
       events: async (info, success, failure) => {
         try {
